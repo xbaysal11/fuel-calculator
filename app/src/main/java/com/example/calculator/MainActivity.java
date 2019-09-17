722e4d3b919da93415e1049d5589b663d7f0bd24
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-09-17 19:57:02 +0600

    ADDED implementation functionality of app

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,35 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
     Spinner measurementSpinner;
     Spinner currencySpinner;
-
+    private TextView result;
+    private EditText distance, price, fuel;
+    private Button calculateButton;
+    private int Result = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        measurementSpinner = findViewById(R.id.measurementSpinner);
+        currencySpinner = findViewById(R.id.currencySpinner);
+        distance = findViewById(R.id.distance);
+        fuel = findViewById(R.id.fuel);
+        price = findViewById(R.id.price);
+        calculateButton = findViewById(R.id.calculateButton);
+        result = findViewById(R.id.result);
+
         //measurement
-        measurementSpinner = findViewById(R.id.measurement);
-
         ArrayAdapter<CharSequence> measurementAdapter = ArrayAdapter.createFromResource(this, R.array.measurement, android.R.layout.simple_spinner_item);
-
         measurementAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         measurementSpinner.setAdapter(measurementAdapter);
 
         //currency
-        currencySpinner = findViewById(R.id.currency);
-
         ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter.createFromResource(this, R.array.currency, android.R.layout.simple_spinner_item);
-
         currencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         currencySpinner.setAdapter(currencyAdapter);
+
+        final Integer selectedMeasurement = measurementSpinner.getSelectedItemPosition();
+        final Integer selectedCurrency = currencySpinner.getSelectedItemPosition();
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int distanceInt = Integer.parseInt(distance.getText().toString());
+                int priceInt = Integer.parseInt(price.getText().toString());
+                int fuelInt = Integer.parseInt(fuel.getText().toString());
+
+                Result = distanceInt + priceInt + fuelInt;
+
+                    result.setText(String.valueOf(Result));
+            }
+        });
     }
 
 }
